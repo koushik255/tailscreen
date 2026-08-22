816bd7cb86d8fa0dddb49f5b32a3b5df88ecfa46
koushik <koushikkheda217@gmail.com>
2026-08-22 15:44:26 -0400

    Fix blank full-page player

--- web/service-worker.ts
+++ web/service-worker.ts
@@ -1,8 +1,8 @@
 /// <reference lib="webworker" />
 
 const worker = globalThis as unknown as ServiceWorkerGlobalScope;
-const CACHE = "tailscreen-shell-v8";
-const SHELL = ["/", "/styles.css?v=8", "/app.js?v=8", "/icon.svg", "/apple-touch-icon.png", "/manifest.webmanifest"];
+const CACHE = "tailscreen-shell-v9";
+const SHELL = ["/", "/styles.css?v=9", "/app.js?v=9", "/icon.svg", "/apple-touch-icon.png", "/manifest.webmanifest"];
 
 worker.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
